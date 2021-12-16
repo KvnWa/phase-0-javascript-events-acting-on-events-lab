9558e6a50b4560bd2d31ab60cacd90969b8d5b3e
Kevin Wang <kevqwang@gmail.com>
2021-12-16 13:39:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ dodger.style.backgroundColor = "#FF69B4";
       const leftNumbers = dodger.style.left.replace("px", "");
       const left = parseInt(leftNumbers, 10);
 
-      if (left > 0) {
+      if (left < 360) {
           dodger.style.left = `${left + 1}px`;
       }
   }
